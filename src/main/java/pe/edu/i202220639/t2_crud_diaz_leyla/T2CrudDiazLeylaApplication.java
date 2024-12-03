55ef383ed8c065aeba586acf5fa02d8e5387c2b3
leyladiaz7 <leyladiazluque@gmail.com>
2024-12-02 23:10:34 -0500

    Carga inicial del T2

---
+++ src/main/java/pe/edu/i202220639/t2_crud_diaz_leyla/T2CrudDiazLeylaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.i202220639.t2_crud_diaz_leyla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2CrudDiazLeylaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2CrudDiazLeylaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/i202220639/t2_crud_diaz_leyla/T2CrudDiazLeylaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.i202220639.t2_crud_diaz_leyla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2CrudDiazLeylaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
